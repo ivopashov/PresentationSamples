1ed6b2c83024002d778a9e8c1d7ee5a7897c7330
ivaylo pashov <ivopashov@abv.bg>
2015-03-06 10:04:10 +0200

    variance and contravariance example

---
+++ VarianceAndContravariance/ConsoleApplication11/ContraVariance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    public class Animal
+    {
+        public void SayYourType()
+        {
+            Console.WriteLine(this.GetType().Name);
+        }
+    }
+
+
+    public class Human : Animal
+    {
+
+    }
+
+    public interface ICallType<in T>
+    {
+        void CallType(T x);
+    }
+
+    public class TypeDiscoverer<T>
+    {
+        T Item;
+        ICallType<T> TypeCaller;
+
+        public TypeDiscoverer(T item, ICallType<T> typeCaller)
+        {
+            Item = item;
+            TypeCaller = typeCaller;
+        }
+
+        public void DiscoverType()
+        {
+            TypeCaller.CallType(Item);
+        }
+    }
+
+    public class AnimalTypeCaller : ICallType<Animal>
+    {
+        public void CallType(Animal x)
+        {
+            x.SayYourType();
+        }
+    }
+}
---
+++ VarianceAndContravariance/ConsoleApplication11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var human = new Human();
+
+
+            // 1. Try removing the in keyword from ICallType - that should be compile time error
+            // 2. delete  new AnimalTypeCaller() and try adding it by hand - the compiler should expect ICallType 
+            //of type Human
+            
+            var humanTypeDiscoverer = new TypeDiscoverer<Human>(human,new AnimalTypeCaller());
+            humanTypeDiscoverer.DiscoverType();
+
+           
+
+        }
+    }
+}
